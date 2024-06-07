b822bcb0225b63b6beede02aa12dff23d4bd97d4
Vivek Bansal <830466@sita.in>
2024-06-07 10:59:04 +0530

    add: pushed result to producer which is listened by code manager

--- src/main/java/com/cce/api/configuration/RabbitConfig.java
+++ src/main/java/com/cce/api/configuration/RabbitConfig.java
@@ -2,6 +2,7 @@ package com.cce.api.configuration;
 
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +10,13 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitConfig {
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        return rabbitTemplate;
+    }
 
     @Bean
     public MessageConverter jsonMessageConverter() {
